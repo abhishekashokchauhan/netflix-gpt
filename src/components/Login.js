a8d487abfb6e9b8f3578ed89f48889e3e309e324
Abhishek Chauhan <abhishekashokchauhan@gmail.com>
2024-09-08 22:17:07 +0530

    Made a sign in, sing up form toggle.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignIn = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
       <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg" />
+        <img
+          alt="logo alter"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg"
+        />
       </div>
       <form className="absolute w-4/12  p-12 bg-black mx-auto my-36 left-0 right-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
@@ -21,8 +38,13 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700"
         />
         <button className="bg-red-600 p-4 my-6 w-full rounded-lg">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="cursor-pointer" onClick={toggleSignIn}>
+          {isSignInForm
+            ? "New to Netflix ? Sign up Now !"
+            : "Already a member ? Sign in Now!"}
+        </p>
       </form>
     </div>
   );
